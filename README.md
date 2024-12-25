f301d38644ffe88d83331ece9a2659e19f856a6f
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-26 01:58:51 +0530

    Strings

---
+++ README.md
@@ -0,0 +1,1 @@
+# This code shows String Functions in Python.
---
+++ strings.py
@@ -0,0 +1,11 @@
+# Functions of Strings
+
+name= "soham is a bad boy"
+print(len(name))
+print(name.endswith("oy"))
+print(name.startswith("os"))
+print(name.capitalize())
+print(name.title())
+
+print(name.count("a"))
+print(name.replace("bad","good").capitalize())
